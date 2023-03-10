d5f90a65d9da5831e626392ea5ead3c8e89146c2
Dimaka <70847277+dimakpa@users.noreply.github.com>
2023-03-10 11:14:05 +0300

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -15,50 +15,50 @@ courses_kb1 = ReplyKeyboardMarkup(
 button_14 = KeyboardButton('6 кубиков')
 button_24 = KeyboardButton('Для начинающих дома / restart')
 button_34 = KeyboardButton('Быстрая сушка')
-button_44 = KeyboardButton('menu')
+button_44 = KeyboardButton('Меню')
 
 courses4_kb4 = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True).add(button_14).add(button_24).add(button_34).add(button_44)
 
 
 # Чек листы/гайды
-button_11 = KeyboardButton('гайд спортивыне добавки')
-button_21 = KeyboardButton('гайд как тренироваться для набора мышц')
-button_31 = KeyboardButton('как тренироваться на рельеф')
-button_41 = KeyboardButton('чек-лист "что мешает похудению"')
-button_51 = KeyboardButton('menu')
+button_11 = KeyboardButton('Гайд спортивыне добавки')
+button_21 = KeyboardButton('Гайд как тренироваться для набора мышц')
+button_31 = KeyboardButton('Как тренироваться на рельеф')
+button_41 = KeyboardButton('Чек-лист "что мешает похудению"')
+button_51 = KeyboardButton('Меню')
 
 course1_kb2 = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True).add(button_11).add(button_21).add(button_31).add(button_41).add(button_51)
 
 # гайд спортивыне добавки
 button_18 = KeyboardButton('Получить гайд спортивыне добавки')
-button_28 = KeyboardButton('menu')
+button_28 = KeyboardButton('Меню')
 course8_kb_guid = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True).add(button_18).add(button_28)
 
 # гайд как тренироваться для набора мышц
 button_19 = KeyboardButton('Получить гайд как тренироваться для набора мышц')
-button_29 = KeyboardButton('menu')
+button_29 = KeyboardButton('Меню')
 course9_kb_guid = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True).add(button_19).add(button_29)
 
 # гайд как тренироваться на рельеф
 button_110 = KeyboardButton('Получить гайд как тренироваться на рельеф')
-button_210 = KeyboardButton('menu')
+button_210 = KeyboardButton('Меню')
 course10_kb_guid = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True).add(button_110).add(button_210)
 
 # чек-лист "что мешает похудению"
 button_111 = KeyboardButton('Получить чек-лист "что мешает похудению"')
-button_211 = KeyboardButton('menu')
+button_211 = KeyboardButton('Меню')
 course11_kb_guid = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True).add(button_111).add(button_211)
 
 # 6 кубиков
-button_13 = KeyboardButton('купить 6 кубиков')
-button_23 = KeyboardButton('о курсе 6 кубиков')
-button_33 = KeyboardButton('menu')
+button_13 = KeyboardButton('Купить 6 кубиков')
+button_23 = KeyboardButton('О курсе 6 кубиков')
+button_33 = KeyboardButton('Меню')
 
 course3_kb2 = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True).add(button_13).add(button_23).add(button_33)
@@ -66,15 +66,15 @@ course3_kb2 = ReplyKeyboardMarkup(
 # дома для начинающих / RESTART
 button_15 = KeyboardButton('Для кого?')
 button_25 = KeyboardButton('Купить RESTART')
-button_35 = KeyboardButton('menu')
+button_35 = KeyboardButton('Меню')
 
 course5_kb2 = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True).add(button_15).add(button_25).add(button_35)
 
 # быстрая сушка
 button_16 = KeyboardButton('Быстрая сушка - для кого?')
-button_26 = KeyboardButton('купить быструю сушку')
-button_36 = KeyboardButton('menu')
+button_26 = KeyboardButton('Купить быструю сушку')
+button_36 = KeyboardButton('Меню')
 
 course6_kb2 = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True).add(button_16).add(button_26).add(button_36)
@@ -82,7 +82,7 @@ course6_kb2 = ReplyKeyboardMarkup(
 # Марафон 21 день
 button_17 = KeyboardButton('О марафоне')
 button_27 = KeyboardButton('Хочу участвовать!')
-button_37 = KeyboardButton('menu')
+button_37 = KeyboardButton('Меню')
 
 course7_kb2 = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True).add(button_17).add(button_27).add(button_37)
